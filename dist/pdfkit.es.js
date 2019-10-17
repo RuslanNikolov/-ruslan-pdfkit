d5decc3d6d6aaf2a7beec4526e42da0102a17e95
- Pocko <Ruslan_Nikolov@epam.com>
2019-10-17 10:25:28 +0300

    .

--- dist/pdfkit.es.js
+++ dist/pdfkit.es.js
@@ -4479,7 +4479,7 @@ var AnnotationsMixin = {
         options.Subtype = "Link";
         options.A = this.ref({
             S: "GoTo",
-            D: [new String(name), "XYZ", null, null, null]
+            D: new String(name)
         });
         options.A.end();
         return this.annotate(x, y, w, h, options);
